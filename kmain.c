910375da299b6483904377067c9d5f8b8105d88d
Alexander Supertramp <alexandersupertramp@gmail.com>
2012-12-11 10:15:56 +0200

    The source code for the first lab

---
+++ kmain.c
@@ -0,0 +1,17 @@
+int main( void )
+{
+    char* vidmem = (char *) 0xb8000;
+    vidmem[0] = 'H';
+    vidmem[1] = 0x7;
+    vidmem[2] = 'e';
+    vidmem[3] = 0x7;
+    vidmem[4] = 'l';
+    vidmem[5] = 0x7;
+    vidmem[6] = 'l';
+    vidmem[7] = 0x7;
+    vidmem[8] = '0';
+    vidmem[9] = 0x7;
+    
+    
+    for(;;); /* Keep the OS running */
+}
